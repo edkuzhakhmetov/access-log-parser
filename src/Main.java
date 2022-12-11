0cf0ea3808f0b5a8b2f205e1a567b68ab9bc142c
EK <eduardkuzhakhmetov@gmail.com>
2022-12-12 01:08:06 +0500

    Курсовой проект File

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,35 @@
 import java.util.Scanner;
+import java.io.File;
 
 public class Main {
-    public static void main(String[] args)
-    {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in) .nextLine();
-        System.out.println("Длина текста: " + text.length());
+    public static void main(String[] args) {
+        //получить входящие данные
+        String path = "";
+        File file;//обязательно
+        boolean fileExists;//обязательно
+        boolean isDirectory;
+        int i=0;
+        for (; ; ) {
+            System.out.println("Введите путь:");
+            path = new Scanner(System.in).nextLine();
+            file = new File(path);
+            isDirectory=file.isDirectory();
+            fileExists = file.exists();
+            //выполнить операции
+            if (isDirectory){
+                System.out.println("указанный путь является путём к папке, а не к файлу");
+                continue;
+            }
+            if (isDirectory==false&&fileExists==false) {
+                System.out.println("файл/папка не существует");
+                continue;
+            }
+            if (fileExists) {
+                System.out.println("Путь указан верно");
+                System.out.println("Это файл номер "+(++i));
+            }
+        }
     }
+
 }
 
